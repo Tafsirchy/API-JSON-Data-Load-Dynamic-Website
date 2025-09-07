b560c405d2fe88e8a128a02ccdefaf22b347e9c8
Tafsirchy <tafsirchy1000@gmail.com>
2025-09-07 10:07:32 +0600

    todo list

---
+++ todo.js
@@ -0,0 +1,37 @@
+const loadTodo = () => {
+    const url = "https://jsonplaceholder.typicode.com/todos";
+
+    fetch(url)
+        .then(response => response.json())
+        .then(response => displayTodo(response))
+}
+
+// {
+//     "userId": 10,
+//     "id": 191,
+//     "title": "temporibus atque distinctio omnis eius impedit tempore molestias pariatur",
+//     "completed": true
+// }
+
+displayTodo = (todos) => {
+    // 1. get the container
+    const todoContainer = document.getElementById("todo-container");
+    // 2. empty the container
+    todoContainer.innerHTML = "";
+    // 3. loop through the array
+    todos.forEach(todo => {
+        // 4. create a div for each todo
+        const todoCard = document.createElement("div")
+        // 5. set the content
+        todoCard.innerHTML = `
+        <div class="toto-card">
+            <p>${todo.completed == true ? `<i class="fa-solid fa-square-check"></i>` : `<i class="fa-regular fa-square-check"></i> </p>`} 
+            
+            <h4>${todo.title}</h4>
+        </div>`
+        // 6. append the div to the container
+        todoContainer.appendChild(todoCard)
+    })
+}
+
+loadTodo()
